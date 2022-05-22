f0b54eeb48221af0ef0e12df80bdcfcb2ddaa447
manhweird20112000 <dinhmanh20112000@gmail.com>
2022-05-22 20:21:24 +0700

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,7 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { LogService } from './utils/logger/log.service';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  // constructor(private readonly logService: LogService) {
-  //   this.logService.log('App Service');
-  // }
-
   getHello() {
     return { message: '你 好.' };
   }
