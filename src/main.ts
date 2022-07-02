f4a96574901d547e19d580dc2f814e9760efb8d0
hs.weird <dinhmanh20112000@gmail.com>
2022-07-03 01:03:25 +0700

    add document

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 // import { HttpExceptionFilter } from './utils/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Kaiju')
+    .setDescription('The kaiju API description')
+    .setVersion('0.1')
+    .addTag('kaiju')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('document', app, document);
   // app.useGlobalInterceptors(new HttpExceptionFilter());
   await app.listen(3000);
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Request, Response } from 'express';
+import { ValidationPipe } from 'src/utils/validation/validation.service';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
-
+@ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @ApiOperation({ summary: 'User đăng ký tài khoản.' })
+  @Post('register')
+  async register(
+    @Res() res: Response,
+    @Req() req: Request,
+    @Body(new ValidationPipe()) createUserDto: CreateUserDto,
+  ) {}
 }
